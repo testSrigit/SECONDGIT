f06f5898e32cb480459a2cf260e9974daca7d8ed
Sri1 <srisowj4linux@gmail.com>
2016-04-09 10:49:57 -0700

    Commiting the code again to codeBase...

---
+++ codeBase/src/SECOND.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : SECOND.c
+ Author      : Sri
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
+int display()
+{
+	printf("This is the second phase release");
+	return 0;
+}
---
+++ codeBase/src/samp2.c
@@ -0,0 +1,11 @@
+int add(int a,int b)
+{
+	printf("Additional changes ...\n");
+	return a+b;
+}
+
+
+int sub(int a,int b)
+{
+	return a-b;
+}
